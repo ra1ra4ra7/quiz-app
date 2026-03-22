54cab0fd0693d4db7928b0b4c700ffdc3b3444c2
Rachel R <rachelravikumar74@gmail.com>
2026-03-23 01:18:15 +0530

    fix case sensitivity issue

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const Score = require("./models/score");
+const Score = require("./models/Score");
 
 const app = express();
 
