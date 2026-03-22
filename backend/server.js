2898ab4566be443199bb7a5351b089cb96296c90
Rachel R <rachelravikumar74@gmail.com>
2026-03-22 18:08:46 +0530

    mongodb final

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,18 @@ const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '../.env') });
 
 const app = express();
+const { MongoClient } = require("mongodb");
+
+const uri = mongodb+srv://quizuser:123@cluster0.9soskvd.mongodb.net/?appName=Cluster0
+
+const client = new MongoClient(uri);
+let db;
+async function connectDB() {
+  await client.connect();
+  db = client.db("quizDB");
+  console.log("MongoDB connected");
+}
+connectDB();
 app.use(cors());
 app.use(express.json());
 
@@ -33,6 +45,11 @@ app.get('/api/scores', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
+
+app.get("/scores", async (req, res) => {
+  const data = await db.collection("scores").find().toArray();
+  res.json(data);
+});
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.get("/", (req, res) => {
@@ -41,13 +58,10 @@ app.get("/", (req, res) => {
 
 let scores = []; // temporary storage
 
-app.post("/submit-score", (req, res) => {
+app.post("/submit-score", async (req, res) => {
   const { name, score } = req.body;
 
-  scores.push({ name, score });
+  await db.collection("scores").insertOne({ name, score });
 
   res.json({ message: "Score saved!" });
-}); 
-app.get("/scores", (req, res) => {
-  res.json(scores);
 });
