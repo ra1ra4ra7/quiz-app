b4994c57f5e2f1de5ef143ec1444eb372bd7bf17
Rachel R <rachelravikumar74@gmail.com>
2026-03-22 20:15:26 +0530

    fix server

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,9 @@
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const path = require('path');
-require('dotenv').config({ path: path.join(__dirname, '../.env') });
 
 const app = express();
-const { MongoClient } = require("mongodb");
-
-const uri = mongodb+srv://quizuser:123@cluster0.9soskvd.mongodb.net/?appName=Cluster0
-
-const client = new MongoClient(uri);
-let db;
-async function connectDB() {
-  await client.connect();
-  db = client.db("quizDB");
-  console.log("MongoDB connected");
-}
-connectDB();
 app.use(cors());
 app.use(express.json());
 
@@ -33,35 +20,25 @@ const scoreSchema = new mongoose.Schema({
 const Score = mongoose.model('Score', scoreSchema);
 
 app.post('/api/scores', async (req, res) => {
-  const { name, score } = req.body;
-  const newScore = new Score({ name, score });
-  await newScore.save();
-  res.json({ message: 'Score saved!' });
+  try {
+    const { name, score } = req.body;
+    const newScore = new Score({ name, score });
+    await newScore.save();
+    res.json({ message: 'Score saved!' });
+  } catch(err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 app.get('/api/scores', async (req, res) => {
-  const scores = await Score.find().sort({ score: -1 }).limit(10);
-  res.json(scores);
+  try {
+    const scores = await Score.find().sort({ score: -1 }).limit(10);
+    res.json(scores);
+  } catch(err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 const PORT = process.env.PORT || 5000;
-
-app.get("/scores", async (req, res) => {
-  const data = await db.collection("scores").find().toArray();
-  res.json(data);
-});
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-app.get("/", (req, res) => {
-  res.send("Backend is running 🚀");
-});app.use(express.json());
-
-let scores = []; // temporary storage
-
-app.post("/submit-score", async (req, res) => {
-  const { name, score } = req.body;
-
-  await db.collection("scores").insertOne({ name, score });
-
-  res.json({ message: "Score saved!" });
-});
+```
